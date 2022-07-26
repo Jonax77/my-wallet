d066df0c556faf74e5ca060880c58b7461caa345
Jonax77 <961843227@qq.com>
2022-07-26 10:01:57 +0800

    loadContract

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider";
+import { loadContract } from "./utils/loadContract";
 
 function App() {
     const [web3Api, setWeb3Api] = useState({
         provider: null,
         web3: null,
+        contract: null,
     });
 
     const [account, setAccount] = useState(null);
 
     const loadProvider = async () => {
         const provider = await detectEthereumProvider();
+        const contract = await loadContract("MyWallet", provider);
 
         if (provider) {
             setWeb3Api({
                 web3: new Web3(provider),
                 provider,
+                contract,
             });
         } else {
             console.error("Please install Metamask");
