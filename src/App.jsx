088aea214df6af5c418fab8c5a8ea55d668faf59
Jonax77 <961843227@qq.com>
2022-07-26 08:12:42 +0800

    getAccount and modify some style

--- src/App.jsx
+++ src/App.jsx
@@ -8,44 +8,57 @@ function App() {
         web3: null,
     });
 
-    useEffect(() => {
-        const loadProvider = async () => {
-            let provider = null;
+    const [account, setAccount] = useState(null);
 
-            // check available provider
-            if (window.ethereum) {
-                // newer version of metamask
-                provider = window.ethereum;
+    const loadProvider = async () => {
+        let provider = null;
 
-                // try to connect to metamask automatically
-                // try {
-                //     await provider.enable();
-                // } catch {
-                //     console.error("User denied accounts access!");
-                // }
-            } else if (window.web3) {
-                provider = window.web3.currentProvider; // this condition is for legacy version of metamask
-            } else if (!process.env.production) {
-                provider = new Web3.providers.HttpProvider(
-                    "http://localhost:7545"
-                ); // Ganache
+        // check available provider
+        if (window.ethereum) {
+            // for newer version of metamask
+            provider = window.ethereum;
+
+            // try to connect to metamask automatically
+            try {
+                await provider.request({ method: "eth_requestAccounts" });
+            } catch {
+                console.error("User denied accounts access!");
             }
+        } else if (window.web3) {
+            provider = window.web3.currentProvider; // this condition is for legacy version of metamask
+        } else if (!process.env.production) {
+            provider = new Web3.providers.HttpProvider("http://localhost:7545"); // Ganache
+        }
 
-            setWeb3Api({
-                web3: new Web3(provider),
-                provider,
-            });
-        };
+        setWeb3Api({
+            web3: new Web3(provider),
+            provider,
+        });
+    };
 
+    const getAccount = async () => {
+        const accounts = await web3Api.web3.eth.getAccounts();
+        setAccount(accounts[0]);
+    };
+
+    useEffect(() => {
         loadProvider();
     }, []);
 
-    console.log(web3Api.web3);
+    useEffect(() => {
+        web3Api.web3 && getAccount(); // only when web3 exists call this function
+    }, [web3Api.web3]); // reload when web3 changes
 
     return (
         <>
             <div className="faucet-wrapper">
                 <div className="faucet">
+                    <div className="account">
+                        <span>
+                            <strong>Account:</strong>
+                        </span>
+                        <div>{account ? account : "Not Connected"}</div>
+                    </div>
                     <div className="balance">
                         Current Balance <strong>10</strong> ETH
                     </div>
